5243e9ac1a8391a17ba9437c2233060662f38d3a
Saviour1001 <harshghodkar@gmail.com>
2024-01-24 21:43:40 +0530

    started writing tests

--- test/WormholePitstop.t.sol
+++ test/WormholePitstop.t.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {WormholePitstop} from "../src/WormholePitstop.sol";
 
-contract WormholePitstopTest is Test {
+import "wormhole-solidity-sdk/testing/WormholeRelayerTest.sol";
+
+contract WormholePitstopTest is WormholeRelayerBasicTest {
     WormholePitstop public wormholePitstop;
 
-    function setUp() public {
-        wormholePitstop = new WormholePitstop();
+    function setUpSource() public override {
+        wormholePitstop = new WormholePitstop(
+            address(relayerSource),
+            address(tokenBridgeSource),
+            address(wormholeSource)
+        );
     }
-}
 
+    function setUpTarget() public override {}
+}
