e6992805da217cd410ba3a4b52891795269bf322
Saviour1001 <harshghodkar@gmail.com>
2024-01-25 01:26:03 +0530

    added a test

--- test/WormholePitstop.t.sol
+++ test/WormholePitstop.t.sol
@@ -8,11 +8,37 @@ import "wormhole-solidity-sdk/testing/WormholeRelayerTest.sol";
 
 contract WormholePitstopTest is WormholeRelayerBasicTest {
     WormholePitstop public wormholePitstop;
+    WormholePitstop public wormholePitstopTarget;
 
     function setUpSource() public override {
         wormholePitstop =
             new WormholePitstop(address(relayerSource), address(tokenBridgeSource), address(wormholeSource));
     }
 
-    function setUpTarget() public override {}
+    function setUpTarget() public override {
+        wormholePitstopTarget =
+            new WormholePitstop(address(relayerTarget), address(tokenBridgeTarget), address(wormholeTarget));
+    }
+
+    function testQuotePitstop() public view {
+        WormholePitstop.PitstopInfo[] memory pitstopInfo = new WormholePitstop.PitstopInfo[](1);
+        pitstopInfo[0] = WormholePitstop.PitstopInfo({targetChain: 2, amount: 100});
+        uint256 totalFees = wormholePitstop.quotePitstop(pitstopInfo);
+        console.log("totalFees", totalFees);
+    }
+
+    function testUsePitstop() public payable {
+        WormholePitstop.PitstopInfo[] memory pitstopInfo = new WormholePitstop.PitstopInfo[](1);
+        pitstopInfo[0] = WormholePitstop.PitstopInfo({targetChain: 2, amount: 100});
+        uint256 totalFees = wormholePitstop.quotePitstop(pitstopInfo);
+
+        console.log("totalFees", totalFees);
+
+        wormholePitstop.usePitstop{value: totalFees+ 100000}(pitstopInfo, address(this));
+
+        // wormholePitstop.usePitstop{value: 1000}(pitstopInfo, address(this));
+    }
+    
+
+
 }
